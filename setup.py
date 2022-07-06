97786b6eff32d4a1afb76fb89226e8f3180b515a
euxhenh <ehasanaj@cs.cmu.edu>
2022-07-05 20:56:03 -0700

    Version update

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ options = {
         set, counting multiplicities)."""
     ),
     'license': 'MIT',
-    'version': '0.7',
+    'version': '0.8',
     'author': 'Euxhen Hasanaj',
     'author_email': 'ehasanaj@cs.cmu.edu',
     'url': 'https://github.com/euxhenh/multiset_multicover',
